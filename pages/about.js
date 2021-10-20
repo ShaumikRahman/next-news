8ad2604355e62a8b9501648be4ad659837972b61
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-10-20 21:01:00 +0100

    refined content and styles for main and about pages

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,31 @@
+import Head from "next/head";
+import styles from "../styles/About.module.css";
+import Link from "next/link";
 
 export const about = () => {
-    return (
-        <div className="container">
-            <h1>About</h1>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About page" />
+      </Head>
+      <div className={styles.about}>
+        <h1 className={styles.title}>About</h1>
+        <h3 className={styles.text}>
+          This is my first project using Next.js, I decided to make a news
+          appliction using{" "}
+          <Link href="https://newsapi.org/">
+            <a className={styles.link}>News API </a>
+          </Link>
+          to practice using the framework and learn its different data fetching
+          methods as well as using CSS modules.
+        </h3>
+        <h3 className={styles.text}>
+            Check my other projects <Link href="https://ShaumikRahman.github.io"><a className={styles.link}>Here</a></Link>
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-export default about
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import breaking from "../img/breaking.jpg";
 
-export default function Home() {
+export default function Home({ articles, results, status }) {
+  console.log(articles);
+
   return (
     <div className="container">
-      <h1 className="title">Hello world</h1>
-      <p className="text">Sample text</p>
+      <Head>
+        <title>Next News</title>
+        <meta name="description" content="The best source for news" />
+      </Head>
+      <h1 className={styles.title}>Next News</h1>
+      <p className={styles.results}>Found {results} articles</p>
+      <div className={styles.articles}>
+        {status === "ok" &&
+          articles.map((article, index) => {
+            return (
+              <div className={styles.article} key={index}>
+                <h1 className="article__title">{article.title}</h1>
+                <div className={styles.small}>
+                {!!article.publishedAt && <p className={styles.date}>{article.publishedAt}</p>}
+                {!!article.author && <p className={styles.author}>{article.author}</p>}
+                </div>
+                {article.urlToImage ? <img src={article.urlToImage} alt="test" /> : <Image src={breaking} />}
+                {!!article.description && <h3 className="article__desc">{article.description}</h3>}
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async (pageContext) => {
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=gb",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    props: {
+      articles: data.articles,
+      results: data.totalResults,
+      status: data.status
+    },
+  };
+};
